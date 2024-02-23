e326066ef9e80c7b02f6e8080e30e4d0856a3a2f
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-02-23 08:38:25 -0800

    connfiguration

---
+++ bin/learn_dart_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:learn_dart_flutter/learn_dart_flutter.dart' as learn_dart_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${learn_dart_flutter.calculate()}!');
+}
