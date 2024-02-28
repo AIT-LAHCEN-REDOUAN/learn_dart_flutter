6f2dc98b8fe9373216fe69bef413352b7b8bb392
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-02-28 06:59:29 -0800

    Variables in DART

--- bin/learn_dart_flutter.dart
+++ bin/learn_dart_flutter.dart
@@ -1,5 +1,89 @@
-import 'package:learn_dart_flutter/learn_dart_flutter.dart' as learn_dart_flutter;
+
+
+import 'dart:ffi';
 
 void main(List<String> arguments) {
-  print('Hello world: ${learn_dart_flutter.calculate()}!');
+
+  int a = 10 ;
+  String b = "Little Nigga";
+  var list = [1,2,5,6,8,7,9];
+
+  for(int i = 0 ; i<list.length;i++){
+    print(list[i]/2*2+2-10);
+    print(++list[i]);
+    print(list[i]++);
+    print(--list[i]);
+    print(list[i]--);
+  }
+
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+
+ //Operational Operator
+
+  print(a+=10);
+  print(a-=10);
+  print(a*=10);
+  print(a ~/=10);
+  print(a%10);
+
+  //Relational Operator
+
+
+  var t  = 20 ;
+  var r = 10  ;
+  print(t==r);
+  print(t<r);
+  print(t>r);
+  print(t>=r);
+  print(t<=r);
+  print(t != r);
+
+
+  //type Test Operatror
+
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+
+  print(t is int);
+  print(b is! String);
+
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+  print("//////////////////////////////");
+
+
+  var bol1 = false  ;
+  var bol2 = true ;
+
+  print(bol1 || bol2);
+
+
+  //Ternary Operator (Logical Operator)
+
+  (10>20)?print("Good Job !!"):print("GET OUT OF MY SIDE I ATTACK YOU DIRECTLY ");
+
+  var myage = null ;
+
+  var myage2 = 30 ;
+
+  var total = myage??myage2;
+  print(total);
+
+
+
 }
