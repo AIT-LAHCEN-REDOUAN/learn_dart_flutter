25304977a4fc00818c275d0bd7c25f151c283b82
AIT-LAHCEN-REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2024-03-07 09:55:07 -0800

    Loops and Functions

---
+++ bin/ControlFlow.dart
@@ -0,0 +1,28 @@
+
+void main(List<String> Args){
+
+  var age =20 ;
+  if(age == 20){
+   print("Welcome");
+  }
+  else if(age>80){
+    print("GO TO SPORT !!");
+  }
+  else{
+    print("GO TO GYM !!");
+  }
+
+  switch(age){
+    case 1:
+      print("Hello Nigga !!");
+      break ;
+    case 2:
+      print("YOU") ;
+      break ;
+    default :
+      print("TARISHED");
+      break;
+  }
+
+}
+
---
+++ bin/Loops.dart
@@ -0,0 +1,47 @@
+void main(List<String> Args){
+
+
+  //For
+  for(int i=0;i<10;i++){
+    print("Hello ");
+  }
+
+
+  print("/////////");
+  print("/////////");  print("/////////");
+  print("/////////");  print("/////////");
+
+  var arr1 = [100,200,300,400,500];
+  for(var s in arr1 ){
+    print(s);
+  }
+
+  print("/////////");
+  print("/////////");  print("/////////");
+  print("/////////");  print("/////////");
+
+
+
+  //While
+  int a = 0 ;
+  while( a<10){
+    print("FOOOL");
+    a++ ;
+  }
+
+
+  /*while(true){
+    print("ggg");
+  }*/
+
+
+  //Do While
+
+  /*do{
+    print("YO");
+
+  }while(true);*/
+
+  bool checkValue = 50>60 ;
+  print(checkValue);
+}
